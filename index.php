a6db23038aee6b1c047562877ddc51b5df5f42ee
Budvin Chathura <bc@example.com>
2019-02-25 19:27:28 +0530

    bootstrap.php added

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Welcome!!
+<?php
+    session_start();
+    define('DS', DIRECTORY_SEPARATOR);
+    define ('ROOT', dirname(__FILE__));
+    // echo DS;
+    echo ROOT;
+    // echo $_SERVER['PATH_INFO'];
+    $url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/')):[];
+    // var_dump($url);
+    require_once(ROOT.DS.'core'.DS.'bootstrap.php');
