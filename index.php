a8ecddc5e33ceb1209a25c6a3b10f424d6a870eb
Budvin Chathura <bc@example.com>
2019-03-04 01:42:39 +0530

    bootstrap.php deleted

--- index.php
+++ index.php
@@ -1,10 +1,33 @@
 <?php
-    session_start();
-    define('DS', DIRECTORY_SEPARATOR);
-    define ('ROOT', dirname(__FILE__));
-    // echo DS;
-    // echo ROOT;
-    // echo $_SERVER['PATH_INFO'];
-    $url = isset($_SERVER['PATH_INFO']) ? explode('/',ltrim($_SERVER['PATH_INFO'],'/')):[];
-    // var_dump($url);
-    require_once(ROOT.DS.'core'.DS.'bootstrap.php');
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', dirname(__FILE__));
+
+//load configurations and helper functions
+require_once(ROOT . DS . 'config' . DS . 'config.php');
+require_once(ROOT . DS . 'app' . DS . 'lib' . DS . 'helpers' . DS . 'functions.php');
+
+//autoload classes
+function myAutoloader($className)
+{
+    $filePath1 = ROOT . DS . 'core' . DS . $className . '.php';
+    $filePath2 = ROOT . DS . 'app' . DS . 'controllers' . DS . $className . '.php';
+    $filePath3 = ROOT . DS . 'app' . DS . 'models' . DS . $className . '.php';
+
+    if (file_exists($filePath1)) {
+        require_once($filePath1);
+    } elseif (file_exists($filePath2)) {
+        require_once($filePath2);
+    } elseif (file_exists($filePath3)) {
+        require_once($filePath3);
+    } else {
+        echo 'not found';
+    }
+}
+
+spl_autoload_register('myAutoloader');
+
+session_start();
+$url = isset($_SERVER['PATH_INFO']) ? explode('/', ltrim($_SERVER['PATH_INFO'], '/')) : [];
+
+//Route the request
+Router::route($url); 
