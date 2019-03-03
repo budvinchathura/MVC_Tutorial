88e23104e5df8b67c8110b96961a7611c8af04b9
Budvin Chathura <bc@example.com>
2019-03-03 14:37:49 +0530

    View class, Controller class added

--- config/config.php
+++ config/config.php
@@ -1,3 +1,8 @@
 <?php
-
+define('DEBUG',true);
 define('DEFAULT_CONTROLLER','Home');
+define('DEFAULT_LAYOUT','default');
+
+define('SROOT','/MVC_Tutorial/'); //server root
+
+define('SITE_TITLE','my first mvc framework');
--- core/Application.php
+++ core/Application.php
@@ -7,8 +7,18 @@ class Application{
         
     }
 
+    //configure to show errors for degbugging
     private function _set_reporting(){
-        if (DEBUG)
+        if (DEBUG){
+            error_reporting(E_ALL);
+            ini_set('display_errors',1);
+        }else{
+            error_reporting(0);
+            ini_set('display_errors',0);
+            ini_set('log_errors',1);
+            ini_set('error_log',ROOT.DS.'tmp'.DS.'logs'.DS.'errors.log');
+
+        }
     }
 
     private function _unregister_globals(){
